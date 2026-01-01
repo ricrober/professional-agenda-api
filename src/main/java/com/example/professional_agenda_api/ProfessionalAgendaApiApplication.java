e8fef117822ac40fd7e1c4dadf5db225be311961
ricrober <11838828+ricrober@users.noreply.github.com>
2025-12-31 22:33:07 -0600

    chore: bootstrap spring boot project

---
+++ src/main/java/com/example/professional_agenda_api/ProfessionalAgendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.professional_agenda_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalAgendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalAgendaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/professional_agenda_api/ProfessionalAgendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.professional_agenda_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalAgendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
